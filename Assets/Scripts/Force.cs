e543632d83c0b895fb966ffc9ba772b6b6f7f891
Washigeri <malek.elouerghi@gmail.com>
2017-12-09 23:08:31 -0500

    phyisique newtonienne

---
+++ Assets/Scripts/Force.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Force
+{
+    public Vector3 applicationPoint;
+    public Vector3 forceVector;
+    public bool persistent = false;
+
+    public Force(float x, float y, float z, float xpos, float ypos, float zpos, bool persistent)
+    {
+        this.applicationPoint = new Vector3(xpos, ypos, zpos);
+        this.forceVector = new Vector3(x, y, z);
+        this.persistent = persistent;
+    }
+}
---
+++ Assets/Scripts/PhysicsManger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PhysicsManger : MonoBehaviour
+{
+    public static Vector3 gravity = new Vector3(0, -9.81f, 0);
+    public static int gravityMultiplier = 1;
+
+    [HideInInspector]
+    public static Vector3 gravityMultiplied = gravity * gravityMultiplier;
+
+    // Use this for initialization
+    private void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
